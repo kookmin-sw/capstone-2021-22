00ac6bdec3e43bfe58cca8fc802048f9fa7d5580
Song-Junho97 <devdream1117@gmail.com>
2021-03-12 22:49:06 +0900

    connect DB

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const session = require('express-session');
+
+const { sequelize } = require('./models');
+
 
 const baseRouter = require('./routers/baseRouter');
 
 const app = express();
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+  app.use(express.json());
+  app.use(express.urlencoded({extended:false}));
+  app.use(session({
+      resave:false,
+      saveUninitialized:false,
+      secret:'cookie',
+      cookie:{
+          httpOnly:true,
+          secure: false,
+      },
+  }));
+
+app.use('/', baseRouter);
 
 app.set('port', process.env.PORT || 8003);
 app.use(express.json());
