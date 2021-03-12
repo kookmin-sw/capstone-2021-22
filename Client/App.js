dd936d171315a1d1755578acc4650b106af03928
지앙 <gojiwon@jw-ui-MacBookPro.local>
2021-03-12 13:37:38 +0900

    login screen

--- Client/App.js
+++ Client/App.js
@@ -1,20 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { StyleSheet, View, Text, TextComponent } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LoginScreen from './Screen/LoginScreen';
+
+const Stack = createStackNavigator();
 
 class App extends Component {
   render () {
     return (
-      <View>
-        <Text>이게뭐약?</Text>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ 
+            title: '로그인' ,
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+              color: "#707070"
+            },
+          }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+        
     )
   }
 }
