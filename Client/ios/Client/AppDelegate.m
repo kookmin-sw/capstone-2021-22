e0baedec971122b691bc8ad0a58c4b22cb90e4c3
지앙 <gojiwon@jw-ui-MacBookPro.local>
2021-03-26 21:27:47 +0900

    Splash screen

--- Client/ios/Client/AppDelegate.m
+++ Client/ios/Client/AppDelegate.m
@@ -3,6 +3,8 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"  // here
+
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -43,6 +45,9 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [RNSplashScreen show];  // here
+
   return YES;
 }
 
