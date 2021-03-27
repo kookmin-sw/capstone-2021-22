1de85206a3ef9762e65e428aa8074730972c81b3
Song-Junho <devdream1117@gmail.com>
2021-03-27 12:36:38 +0900

    add idCheck in join

--- Server/routers/auth.js
+++ Server/routers/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const {jwtkey} = require('../keys')
+const { jwtkey } = require('../keys')
 const bcrypt = require('bcrypt');
 const { isLoggedIn, isNotLoggedIn } = require('../middlewares/middlewares');
 const USERS = require('../models').USERS;
@@ -10,17 +10,24 @@ const USERS = require('../models').USERS;
 const router = express.Router();
 
 router.post('/join', isNotLoggedIn, async (req, res, next) => {
-  const { name, nick, password } = req.body;
+  const { name, nick, password, isExistId } = req.body;
   try {
-    const hash = await bcrypt.hash(password, 12);
-    await USERS.create({
-      name,
-      nick,
-      password: hash,
-    });
-    res.status(200).json( {
-      "join" : true
-  });
+    if (isExistId) {
+      res.status(401).json({
+        "join": false
+      });
+    }
+    else {
+      const hash = await bcrypt.hash(password, 12);
+      await USERS.create({
+        name,
+        nick,
+        password: hash,
+      });
+      res.status(200).json({
+        "join": true
+      });
+    }
   } catch (error) {
     console.error(error);
     return next(error);
@@ -34,57 +41,57 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
       return next(authError);
     }
     if (!user) {
-      return res.status(422).send({error :`${info.message}`})
+      return res.status(422).send({ error: `${info.message}` })
     }
     return req.login(user, (loginError) => {
       if (loginError) {
         console.error(loginError);
         return next(loginError);
       }
-      const token = jwt.sign({userId:user.id},jwtkey);
-      
+      const token = jwt.sign({ userId: user.id }, jwtkey);
+
       res.status(200).json(
         {
-            "token" : token,
-            "name" : user.name,
-            "pill" : "8"
+          "token": token,
+          "name": user.name,
+          "pill": "8"
         }
-    );
+      );
     });
-  })(req, res, next); 
+  })(req, res, next);
 });
 
 
 
 router.get('/test', passport.authenticate('jwt', { session: false }),
-    (req, res)=> {
-        res.status(200).json( {
-          "success" : true
-      });
-    }
+  (req, res) => {
+    res.status(200).json({
+      "success": true
+    });
+  }
 );
 
-router.post('/idCheck' ,   async (req, res, next) => { 
-    try {
-      const exUser = await USERS.findOne({ where: { nick: req.body.nick } });
-      if(exUser) {
-        res.status(200).json(
-          {
-            "idCheck": true
-          }
-        )
-      }
-      else{
-        res.status(422).json(
-          {
-            "idCheck": false
-          }
-        )
-      }
-    }catch {
-      console.error(error);
-      done(error);
+router.post('/isExistId', async (req, res, next) => {
+  try {
+    const exUser = await USERS.findOne({ where: { nick: req.body.nick } });
+    if (exUser) {
+      res.status(200).json(
+        {
+          "isExistId": true
+        }
+      )
     }
+    else {
+      res.status(422).json(
+        {
+          "isExistId": false
+        }
+      )
+    }
+  } catch {
+    console.error(error);
+    done(error);
+  }
 });
 
 
