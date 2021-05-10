cb36421f918d6319d2f8c2bb7a25f64b6dc5b1bb
Song-Junho97 <devdream1117@gmail.com>
2021-05-11 01:07:34 +0900

    add pill image and make search router

--- Server/routers/search.js
+++ Server/routers/search.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const passport = require('passport');
 const PILLS = require('../models').PILLS;
-
+const { promises: fs } = require("fs");
 const router = express.Router();
+const path = require('path');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const ps ='./test.png';
 
-router.get('/', passport.authenticate('jwt', { session: false }),
-    async (req, res) =>{
-        try {
-            
+router.get('/',  async (req, res) => {
+   
+   const { pillName } = req.body;
 
-        } catch {
-            console.error(error);
-            done(error);
-        }
+
+
+    try {
+
+    const pillInfo = await PILLS.findAll({where : {name:{[Op.like]: `%${pillName}%` }}, raw: true} );
     
+    for(let i = 0;i<Object.keys(pillInfo).length;i++){
+        
+        pillInfo[i].image= await fs.readFile(__dirname + '/images'+'/'+pillInfo[i].id+'.jpg',              //파일 읽기
+             (err, data) =>
+            {
+                console.log(data)
+                return data;
+    
+            }
+        )
+    }
+    
+    
+    res.json(pillInfo);
+
+    }
+    catch {
+        console.error(error);
+         done(error);
+    }
+
+
     });
-  }
-);
+  
+
 
 
 module.exports = router;
