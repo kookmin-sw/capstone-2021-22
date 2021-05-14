476ea18f12ce05a83be8b6adb73783eb2ef05a5d
Song-Junho97 <devdream1117@gmail.com>
2021-05-15 02:23:02 +0900

    complete favorite router

--- Server/routers/favorite.js
+++ Server/routers/favorite.js
@@ -4,13 +4,39 @@ const jwt = require('jsonwebtoken');
 const { jwtkey } = require('../keys')
 const USERS = require('../models').USERS;
 const PILLS = require('../models').PILLS;
+const FAVORITES = require('../models').FAVORITES;
 const router = express.Router();
 
-router.get('/test', passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.status(200).json({
-      "success": true
-    });
+router.post('/', passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    const { pillId, isFavorite } = req.body;
+
+    try {
+    if (isFavorite) {
+
+      await FAVORITES.create({
+        pill_id: parseInt(199303108) ,
+        user_id: parseInt(7),
+      });
+      res.status(200).json({
+        "create": true
+      });
+    }
+    else {
+      await FAVORITES.destroy({where: {
+        pill_id:pillId,
+        user_id:req.user.id
+      }});
+      res.status(200).json({
+        "delete": true
+      });
+    }
+  }
+  catch (error){
+    console.error(error);
+    return next(error);
+  }
+    
   }
 );
 
