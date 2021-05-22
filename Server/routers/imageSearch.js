fc307b50435500cd91b156f5ce8421d9851d58ac
Song-Junho97 <devdream1117@gmail.com>
2021-05-23 00:52:54 +0900

    add upload

--- Server/routers/imageSearch.js
+++ Server/routers/imageSearch.js
@@ -1,4 +1,6 @@
 const express = require('express');
+
+const multer = require('multer');
 const passport = require('passport');
 const { promises: fs } = require("fs");
 const router = express.Router();
@@ -6,25 +8,32 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
-router.post('/',  async (req, res) => {
-
-    
-
-
-
-    try {
-       
-
-        console.log(req);
-
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __dirname+"/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  },
+ });
+ var upload = multer({ storage: storage }).fields([{name:'first'},{name:'second'}])
+ 
+ // Router
+ router.post("/", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
     }
-    catch (error) {
-        console.error(error);
-        done(error);
-    }
-
-
-});
-
-
-module.exports = router;
+    console.log(req.files);
+    const obj = JSON.parse(JSON.stringify(req.files));
+    console.log(obj.first[0].fieldname);
+    return res.json({
+      success: true,
+      image: res.req.files.fisrt,
+      fileName: res.req.files,
+    });
+  });
+ });
+ 
+ 
+ module.exports = router;
