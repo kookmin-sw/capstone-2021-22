973f5fb7af6de12c834697e5a80d960eb119ba7b
Song-Junho97 <devdream1117@gmail.com>
2021-05-24 00:23:45 +0900

    add unlogged pillDetail

--- Server/routers/pillDetail.js
+++ Server/routers/pillDetail.js
@@ -57,6 +57,40 @@ router.post('/', passport.authenticate('jwt', { session: false }), async (req, r
 
 });
 
+router.post('/unLogged',  async (req, res) => {
+
+    const { pillId } = req.body;
+
+    const result = spawn('python', [__dirname + '/crawler/crawl.py', pillId]);
+
+
+
+
+    try {
+        
+        const pillInfo = await PILLS.findOne({ where: { id: pillId }, raw: true });
+
+        pillInfo.image = await fs.readFile(__dirname + '/images' + '/' + pillId + '.jpg',              //파일 읽기
+            (err, data) => {
+
+                return data;
+
+            }
+        )
+
+        pillInfo.isFavorite =  false ;
+        res.json(pillInfo);
+
+
+    }
+    catch (error) {
+        console.error(error);
+        done(error);
+    }
+
+
+});
+
 router.post('/code', async (req, res) => {
 
     const { pillId } = req.body;
